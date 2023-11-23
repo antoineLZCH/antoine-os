cb4f3d3c570254373d8198e88ffb2ade602e2fad
Antoine LOZACH <antoine.lozach@cacd2.fr>
2023-11-23 20:11:30 +0100

    👌 IMPROVE: add Settings window
    Signed-off-by: Antoine LOZACH <antoine.lozach@cacd2.fr>

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,6 @@ const uiStore = useUIStore()
   <div class="p-2 h-full">
     <TheWindow v-if="uiStore.isAboutWindowOpen" name="About" :width="400" :height="600" @close="uiStore.isAboutWindowOpen = false" />
     <TheWindow v-if="uiStore.isPlaygroundWindowOpen" name="Playground" :width="400" :height="500" @close="uiStore.isPlaygroundWindowOpen = false" />
+    <TheWindow v-if="uiStore.isSettingsWindowOpen" name="Settings" :width="200" :height="300" @close="uiStore.isSettingsWindowOpen = false" />
   </div>
 </template>
